a6d60fad67ac195412095b3fffa0189128f2bcb4
HEROGWP <pp820819@gmail.com>
2017-07-10 23:32:06 +0800

    Add  FizzBuzz

--- kata/lib/kata.rb
+++ kata/lib/kata.rb
@@ -1,2 +1,15 @@
-class Kata
+# 1. 3 的倍數回傳 Fizz
+# 2. 5 的倍數回傳 Buzz
+# 3. 15 的倍數回傳 FizzBuzz
+
+class FizzBuzz
+  def self.convert(n)
+    if (n % 15).zero?
+      'FizzBuzz'
+    elsif (n % 3).zero?
+      'Fizz'
+    elsif (n % 5).zero?
+      'Buzz'
+    end
+  end
 end
--- kata/spec/lib/kata_spec.rb
+++ kata/spec/lib/kata_spec.rb
@@ -1,9 +1,20 @@
-require_relative "../../lib/kata"
+require_relative '../../lib/kata'
 
-describe "Kata" do
+describe 'Kata' do
 
-    it "should be passed" do
-        expect(true).to eq(true)
-    end
+  # 1. 3 的倍數回傳 Fizz
+  # 2. 5 的倍數回傳 Buzz
+  # 3. 15 的倍數回傳 FizzBuzz
 
+  it 'should convert 3 to Fizz' do
+    expect(FizzBuzz.convert(3)).to eq 'Fizz'
+  end
+
+  it 'should convert 5 to Buzz' do
+    expect(FizzBuzz.convert(5)).to eq 'Buzz'
+  end
+
+  it 'should convert 15 to FizzBuzz' do
+    expect(FizzBuzz.convert(15)).to eq 'FizzBuzz'
+  end
 end
