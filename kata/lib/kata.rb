816a869a2c6c223c7ae9e77e3fd4554fa7fb4581
HEROGWP <pp820819@gmail.com>
2017-07-10 23:54:00 +0800

    add LeapYear

--- kata/lib/kata.rb
+++ kata/lib/kata.rb
@@ -1,2 +1,12 @@
-class Kata
+# 1. 西元年分除以400可整除，為閏年。
+# 2. 西元年分除以4可整除但除以100不可整除，為閏年。
+
+class LeapYear
+  def self.is_leap?(y)
+    if (y % 400).zero? || ((y % 4).zero? && (y % 100 != 0))
+      true
+    else
+      false
+    end
+  end
 end
--- kata/spec/lib/kata_spec.rb
+++ kata/spec/lib/kata_spec.rb
@@ -1,9 +1,23 @@
-require_relative "../../lib/kata"
+require_relative '../../lib/kata'
 
-describe "Kata" do
+describe 'Kata' do
 
-    it "should be passed" do
-        expect(true).to eq(true)
-    end
+  # 1. 西元年分除以400可整除，為閏年。
+  # 2. 西元年分除以4可整除但除以100不可整除，為閏年。
 
+  example '3 is not leap year' do
+    expect(LeapYear.is_leap?(3)).to eq(false)
+  end
+
+  example '4 is leap year' do
+    expect(LeapYear.is_leap?(4)).to eq(true)
+  end
+
+  example '200 is not leap year' do
+    expect(LeapYear.is_leap?(200)).to eq(false)
+  end
+
+  example '400 is leap year' do
+    expect(LeapYear.is_leap?(400)).to eq(true)
+  end
 end
