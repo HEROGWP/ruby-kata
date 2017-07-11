788e9542d953a3be1f5ef6b4ac52f28661fa9dea
HEROGWP <pp820819@gmail.com>
2017-07-11 14:01:34 +0800

    Add Roman

--- kata/lib/kata.rb
+++ kata/lib/kata.rb
@@ -1,2 +1,29 @@
-class Kata
+class Roman
+  CONVERT_TABLE = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ]
+  def convert(n)
+    result = ''
+
+    CONVERT_TABLE.each do |s, i|
+      while n >= i
+        result << s
+        n -= i
+      end
+    end
+
+    result
+  end
 end
--- kata/spec/lib/kata_spec.rb
+++ kata/spec/lib/kata_spec.rb
@@ -1,9 +1,41 @@
-require_relative "../../lib/kata"
+require_relative '../../lib/kata'
 
-describe "Kata" do
+describe 'Kata' do
+  let(:roman){ Roman.new }
 
-    it "should be passed" do
-        expect(true).to eq(true)
-    end
+  it 'should convert 1 to I' do
+    expect(roman.convert(1)).to eq('I')
+  end
 
+  it 'should convert 4 to IV' do
+    expect(roman.convert(4)).to eq('IV')
+  end
+
+  it 'should convert 5 to V' do
+    expect(roman.convert(5)).to eq('V')
+  end
+
+  it 'should convert 9 to IX' do
+    expect(roman.convert(9)).to eq('IX')
+  end
+
+  it 'should convert 10 to X' do
+    expect(roman.convert(10)).to eq('X')
+  end
+
+  it 'should convert 20 to XX' do
+    expect(roman.convert(20)).to eq('XX')
+  end
+
+  it 'should convert 1954 to MCMLIV' do
+    expect(roman.convert(1954)).to eq('MCMLIV')
+  end
+
+  it 'should convert 1990 to MCMXC' do
+    expect(roman.convert(1990)).to eq('MCMXC')
+  end
+
+  it 'should convert 2014 to MMXIV' do
+    expect(roman.convert(2011)).to eq('MMXIV')
+  end
 end
