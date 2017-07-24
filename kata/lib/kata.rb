0ef083351f902d34fb24bdc70d0bd858ec051d5b
HEROGWP <pp820819@gmail.com>
2017-07-25 01:58:16 +0800

    BowlingGame

--- kata/lib/kata.rb
+++ kata/lib/kata.rb
@@ -1,2 +1,29 @@
-class Kata
+class BowlingGame
+  def initialize
+    @sorce = 0
+    @record = []
+  end
+
+  def roll(n)
+    @sorce += n
+    @record << n
+  end
+
+  def sorce
+    @sorce
+  end
+
+  def roll_many(*n)
+    n.each do |n|
+      unless finished?
+        self.roll(n)
+      end
+    end
+  end
+
+  private
+
+  def finished?
+    @record.size >= 20
+  end
 end
--- kata/spec/lib/kata_spec.rb
+++ kata/spec/lib/kata_spec.rb
@@ -1,9 +1,34 @@
-require_relative "../../lib/kata"
+require_relative '../../lib/kata'
 
-describe "Kata" do
+describe BowlingGame do
+  before do
+    @game = BowlingGame.new
+  end
 
-    it "should be passed" do
-        expect(true).to eq(true)
-    end
+  example 'zero' do
+    expect(@game.sorce).to eq(0)
+  end
 
+  example 'first roll: 1' do
+    @game.roll(1)
+    expect(@game.sorce).to eq(1)
+  end
+
+  example 'first roll: 1, 2, 3' do
+    @game.roll(1)
+    @game.roll(2)
+    @game.roll(3)
+
+    expect(@game.sorce).to eq(6)
+  end
+
+  example 'roll_many' do
+    @game.roll_many(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+    expect(@game.sorce).to eq(20)
+  end
+
+  example 'finished game' do
+    @game.roll_many(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+    expect(@game.sorce).to eq(20)
+  end
 end
