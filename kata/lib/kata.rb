0f955ccfaed60daca162ac2fcf692eeedb7b8b52
HEROGWP <pp820819@gmail.com>
2017-07-25 01:45:33 +0800

    API Services test

--- kata/lib/kata.rb
+++ kata/lib/kata.rb
@@ -1,2 +1,16 @@
-class Kata
+require 'rest_client'
+require 'json'
+
+class TrainClient
+  ENDPOINT = 'http://localhost:3000'
+
+  def query(train_id)
+    response = RestClient.get "#{ENDPOINT}/api/v1/trains/#{train_id}"
+    JSON.parse(response)
+  end
+
+  def reserve(train_id, seat_id, email)
+    response = RestClient.post "#{ENDPOINT}/api/v1/trains/#{train_id}/reservations", seat_id: seat_id, email: email
+    JSON.parse(response)
+  end
 end
--- kata/spec/lib/kata_spec.rb
+++ kata/spec/lib/kata_spec.rb
@@ -1,9 +1,26 @@
-require_relative "../../lib/kata"
+require_relative '../../lib/kata'
+require 'rest_client'
+require 'json'
 
-describe "Kata" do
 
-    it "should be passed" do
-        expect(true).to eq(true)
-    end
+describe TrainClient do
+  before do
+    @client = TrainClient.new
+  end
 
+  it 'should query train data' do
+    train_id = 'Train-123'
+    data = ['1A', '2B']
+
+    expect(RestClient).to receive(:get).with("http://localhost:3000/api/v1/trains/#{train_id}").and_return(JSON.generate(data))
+    expect(@client.query(train_id)).to eq(data)
+  end
+
+  it 'should add reservation' do
+    train_id = 'Train-123'
+    response_data = { 'message' => 'ok' }
+
+    expect(RestClient).to receive(:post).with("http://localhost:3000/api/v1/trains/#{train_id}/reservations", seat_id: '1A', email: 'herogwp').and_return(JSON.generate(response_data))
+    expect(@client.reserve(train_id, '1A', 'herogwp')).to eq(response_data)
+  end
 end
