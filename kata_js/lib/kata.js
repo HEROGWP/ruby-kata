b16f33431b64c8f1c49e01e34ecb45a12da5ec09
tancc <vr2006ccc@gmail.com>
2017-07-25 16:22:26 +0800

    step4-js

--- kata_js/lib/kata.js
+++ kata_js/lib/kata.js
@@ -1,3 +1,25 @@
-class Kata {};
+class BowlingGame {
+  constructor() {
+    this.sorce = 0
+    this.record = []
+  }
+  roll(n) {
+    this.sorce += n
+    this.record.push(n)
+  }
+  sorce() {
+    return this.sorce;
+  }
+  roll_many(ns) {
+    ns.forEach(n => {
+      if (!this.finished()) {
+        this.roll(n)
+      }
+    })
+  }
+  finished() {
+    return this.record.length >= 20
+  }
+};
 
-module.exports = Kata;
+module.exports = BowlingGame;
--- kata_js/spec/lib/kata.spec.js
+++ kata_js/spec/lib/kata.spec.js
@@ -1,7 +1,29 @@
-const Kata = require('../../lib/kata')
+const BowlingGame = require('../../lib/kata')
 
-describe('kata', () => {
-  it('should be passed', () => {
-    expect(true).toBe(true)
+describe('BowlingGame', () => {
+  let game = new BowlingGame()
+  beforeEach(() => {
+    game = new BowlingGame()
+  })
+  it('zero', () => {
+    expect(game.sorce).toBe(0)
+  })
+  it('first roll: 1', () => {
+    game.roll(1)
+    expect(game.sorce).toBe(1)
+  })
+  it('first roll: 1, 2, 3', () => {
+    game.roll(1)
+    game.roll(2)
+    game.roll(3)
+    expect(game.sorce).toBe(6)
+  })
+  it('roll_many', () => {
+    game.roll_many([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    expect(game.sorce).toBe(20)
+  })
+  it('finished game', () => {
+    game.roll_many([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    expect(game.sorce).toBe(20)
   })
 })
