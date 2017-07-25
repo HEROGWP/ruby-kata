c3fe4f4703d0480b2d73609a486a9c605319dba8
tancc <vr2006ccc@gmail.com>
2017-07-25 15:49:53 +0800

    step1-js

--- kata_js/lib/kata.js
+++ kata_js/lib/kata.js
@@ -1,3 +1,17 @@
-class Kata {};
+// 1. 3 的倍數回傳 Fizz
+// 2. 5 的倍數回傳 Buzz
+// 3. 15 的倍數回傳 FizzBuzz
 
-module.exports = Kata;
+class FizzBuzz {
+  static convert(n) {
+    if (n % 15 === 0) {
+      return 'FizzBuzz'
+    } else if (n % 3 === 0) {
+      return 'Fizz'
+    } else if (n % 5 === 0) {
+      return 'Buzz'
+    }
+  }
+}
+
+module.exports = FizzBuzz;
--- kata_js/spec/lib/kata.spec.js
+++ kata_js/spec/lib/kata.spec.js
@@ -1,7 +1,13 @@
-const Kata = require('../../lib/kata')
+const FizzBuzz = require('../../lib/kata')
 
-describe('kata', () => {
-  it('should be passed', () => {
-    expect(true).toBe(true)
+describe('FizzBuzz', () => {
+  it('should convert 3 to Fizz', () => {
+    expect(FizzBuzz.convert(3)).toBe('Fizz')
+  })
+  it('should convert 5 to Fizz', () => {
+    expect(FizzBuzz.convert(5)).toBe('Buzz')
+  })
+  it('should convert 15 to Fizz', () => {
+    expect(FizzBuzz.convert(15)).toBe('FizzBuzz')
   })
 })
