1bd987bbb3bb668373886e2543cd05517730aca1
tancc <vr2006ccc@gmail.com>
2017-07-25 15:55:20 +0800

    step2-js

--- kata_js/lib/kata.js
+++ kata_js/lib/kata.js
@@ -1,3 +1,13 @@
-class Kata {};
+// 1. 西元年分除以400可整除，為閏年。
+// 2. 西元年分除以4可整除但除以100不可整除，為閏年。
 
-module.exports = Kata;
+class LeapYear {
+  static is_leap(y) {
+    if (y % 400 === 0 || (y % 4 === 0 && y % 100 != 0)) {
+      return true
+    }
+    return false
+  }
+};
+
+module.exports = LeapYear;
--- kata_js/spec/lib/kata.spec.js
+++ kata_js/spec/lib/kata.spec.js
@@ -1,7 +1,19 @@
-const Kata = require('../../lib/kata')
+const LeapYear = require('../../lib/kata')
 
-describe('kata', () => {
-  it('should be passed', () => {
-    expect(true).toBe(true)
+describe('LeapYear', () => {
+  // 1. 西元年分除以400可整除，為閏年。
+  // 2. 西元年分除以4可整除但除以100不可整除，為閏年。
+
+  it('3 is not leap year', () => {
+    expect(LeapYear.is_leap(3)).toBe(false)
+  })
+  it('4 is leap year', () => {
+    expect(LeapYear.is_leap(4)).toBe(true)
+  })
+  it('200 is not leap year', () => {
+    expect(LeapYear.is_leap(200)).toBe(false)
+  })
+  it('400 is leap year', () => {
+    expect(LeapYear.is_leap(400)).toBe(true)
   })
 })
